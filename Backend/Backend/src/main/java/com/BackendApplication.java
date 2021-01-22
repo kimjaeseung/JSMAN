a1178ac8d44c2793592604e826abac5a4acba9e7
jsman <jsman9777@gmail.com>
2021-01-22 17:03:02 +0900

    test

--- Backend/Backend/src/main/java/com/BackendApplication.java
+++ Backend/Backend/src/main/java/com/BackendApplication.java
@@ -9,5 +9,5 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-//test 2021.01.22 김재승 17:00 commit test
+//test 2021.01.22 김재승 17:00 commit test 
 }
