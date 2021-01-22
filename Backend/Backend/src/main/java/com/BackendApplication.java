e351dd8d2b5937afd2cb8ba985c94c51459667a8
jsman <jsman9777@gmail.com>
2021-01-22 17:01:22 +0900

    [S04P12A307-59][ADD] feat test

---
+++ Backend/Backend/src/main/java/com/BackendApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+//test 2021.01.22 김재승 17:00 commit test
+}
