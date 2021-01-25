4669db5efa21e9289349578a6c17529a2cdc699c
likelionSungGuk <sung9gi@gmail.com>
2021-01-25 17:09:51 +0900

    [S04P12A307-66] [ADD] feat/Article: 뉴스 기사 상세 보여주기 views/Article.vue  레이아웃 구성

--- newshi/src/App.vue
+++ newshi/src/App.vue
@@ -1,52 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    // HelloWorld
   },
 
   data: () => ({
@@ -54,3 +19,16 @@ export default {
   })
 };
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&family=Noto+Serif+KR:wght@700&display=swap');
+
+html, body {
+  font-family: Spectral, "Noto Serif KR", serif;
+  font-weight: 900;
+}
+
+#app {
+  font-family: Spectral, "Noto Serif KR", serif;
+}
+
+</style>
