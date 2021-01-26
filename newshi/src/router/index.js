1c8e48c1fbd047847fa591e4b13ac7e933cd493d
likelionSungGuk <sung9gi@gmail.com>
2021-01-26 15:31:58 +0900

    [S04P12A307-65] [ADD] feat/main: 메인 페이지 레이아웃 작업 진행중

--- newshi/src/router/index.js
+++ newshi/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Article from "../views/Article.vue";
+import Main from "../views/Main.vue";
 
 Vue.use(VueRouter);
 
@@ -12,18 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/article",
     name: "Article",
-    component: Article
+    component: Article,
+  },
+  {
+    path: "/main",
+    name: "Main",
+    component: Main,
   }
 ];
 
---
+++ newshi/src/views/Main.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-container>
+    <v-toolbar
+      color="white"
+      flat
+      dense
+    >
+      <v-tabs
+        v-model="tab"
+        center-active
+        show-arrows
+        align-with-title
+      >
+        <v-tabs-slider color="grey lighten-4"></v-tabs-slider>
+        <v-tab
+          v-for="curator in curators"
+          :key="curator"
+          style="height: 48px; width: 48px;"
+        >
+          <v-list-item-avatar>
+            <v-img :src="curator"></v-img>
+          </v-list-item-avatar>
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-list>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              v-text="item.header"
+            ></v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-item
+              v-else
+              :key="item.title"
+            >
+              <v-list-item-avatar
+                tile
+              >
+                <v-img :src="item.avatar"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-title v-html="item.subtitle"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data: function () {
+    return {
+        tab: null,
+        curators: [
+          'https://cdn.vuetifyjs.com/images/lists/1.jpg', 
+          'https://cdn.vuetifyjs.com/images/lists/2.jpg', 
+          'https://cdn.vuetifyjs.com/images/lists/3.jpg', 
+          'https://cdn.vuetifyjs.com/images/lists/4.jpg', 
+          'https://cdn.vuetifyjs.com/images/lists/5.jpg'
+        ],
+        items: [
+          // { header: 'Today' },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem vel impedit, animi corporis iure possimus quos dolore reiciendis? Iure animi eos optio, dolor tempore nihil id in odit sunt?',
+            subtitle: 'OO일보'
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem vel impedit, animi corporis iure possimus quos dolore reiciendis? Iure animi eos optio, dolor tempore nihil id in odit sunt?',
+            subtitle: 'OO일보'
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem vel impedit, animi corporis iure possimus quos dolore reiciendis? Iure animi eos optio, dolor tempore nihil id in odit sunt?',
+            subtitle: 'OO일보'
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem vel impedit, animi corporis iure possimus quos dolore reiciendis? Iure animi eos optio, dolor tempore nihil id in odit sunt?',
+            subtitle: 'OO일보'
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+            title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem vel impedit, animi corporis iure possimus quos dolore reiciendis? Iure animi eos optio, dolor tempore nihil id in odit sunt?',
+            subtitle: 'OO일보'
+          },
+        ],
+      }
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 800px;
+}
+</style>
