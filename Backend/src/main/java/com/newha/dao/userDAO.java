88f7834cffa262e8a6a18dea98d68e6f16e22548
jsman <jsman9777@gmail.com>
2021-01-27 14:25:51 +0900

    [S04P12A307-79][ADD] feat/join : 아이디 중복체크 기능 추가

--- Backend/src/main/java/com/newha/dao/userDAO.java
+++ Backend/src/main/java/com/newha/dao/userDAO.java
@@ -1,16 +1,20 @@
 package com.newha.dao;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.newha.vo.User;
 
 public interface UserDAO {
 	public ArrayList<User> selectAll();
-	public void insert(User u);
+	public int insert(User u);
 	public void delete(String id);
 	public void update(User u);
 	public User selectOne(User u);
 	public User selectOneById(String id);
-	
+	public int selectid(String id);
+	public void inserttag(int userNo, String name);
+	public int userNo(String id);
+
 }
  
--- Backend/src/main/java/com/newha/service/UserService.java
+++ Backend/src/main/java/com/newha/service/UserService.java
@@ -1,7 +1,7 @@
 package com.newha.service;
 
 import java.util.ArrayList;
-
+import java.util.Map;
 
 import com.newha.vo.User;
 
@@ -9,9 +9,13 @@ import com.newha.vo.User;
 public interface UserService {
 	
 	public ArrayList<User> selectAll();
-	public void insert(User u);
+	public int insert(User u);
 	public void delete(String id);
 	public void update(User u);
 	public User login(User u);
 	public User userInfo(String id);
+	public int selectid(String id);
+	public void inserttag(int userNo, String name);
+	public int userNo(String id);
+	
 }
