239ec6929f2c9ebcd2559d30a737ac84f2875cd1
whiteburnout <whiteburnout@gmail.com>
2021-01-27 16:27:56 +0900

    '[S04P12A307-71][ADD]feat/search:해쉬태그검색'

--- Backend/src/main/java/com/newha/controller/TagController.java
+++ Backend/src/main/java/com/newha/controller/TagController.java
@@ -1,11 +1,25 @@
 package com.newha.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.newha.service.TagService;
+import com.newha.vo.HashTag;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080")
 public class TagController {
-
 	
+	@Autowired
+	TagService service;
+
+	@GetMapping(value = "/search/tag/{keyword}")
+	List<HashTag> searchTag(@PathVariable String keyword){
+		return service.searchTag(keyword + "%");
+	}
 }
