47ea095480d3a0a3b660479a4652de40965cac3a
jsman <jsman9777@gmail.com>
2021-02-01 10:50:36 +0900

    [S04P13A307-85][UPDATE] feat/user : 회원관련 코드 정리완료

--- Backend/src/main/java/com/newha/controller/UserController.java
+++ Backend/src/main/java/com/newha/controller/UserController.java
@@ -35,117 +35,153 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+//http://localhost/swagger-ui.html
 @Api("UserController V1")
 @RestController
 @CrossOrigin(origins = "http://localhost:8080")
 public class UserController {
-	
+
 	@Autowired
 	public JavaMailSender javaMailSender;
-	
+
 	@Autowired
 	private JwtService jwtService;
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	public static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
-	
+
 	@ApiOperation(value = "유저 리스트 조회", notes = "유저 리스트를 리턴", response = List.class)
 	@GetMapping(value = "/user")
 	public List<User> selectAll() {
 		return service.selectAll();
 	}
-	
+
 	@ApiOperation(value = "아이디 중복검사", notes = "아이디 중복검사결과'success' 또는 'fail' 문자열을 리턴", response = Map.class)
 	@GetMapping(value = "/idcheck/{id}") // 아이디체크
-	public Map<String, String> selectid( @ApiParam(value = "id", required = true)@PathVariable String id) {
+	public ResponseEntity<Map<String, String>> selectid(
+			@ApiParam(value = "id", required = true) @PathVariable String id) {
 		Map<String, String> map = new HashMap<>();
-		System.out.println(id);
+		HttpStatus status = null;
 		int result = service.selectId(id);
-		System.out.println(result);
-		if(result == 0)
+		if (result == 0) {
 			map.put("message", "success"); // 0이면 없는거 1이면 있는거
-		else
+			status = HttpStatus.ACCEPTED;
+		} else {
 			map.put("message", "fail");
-		return map;
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, String>>(map, status);
 	}
+
 	@ApiOperation(value = "이메일 인증", notes = "입력값으로 id(email) 주면 이메일 발송. 리턴값은 confirm: 인증번호 ", response = Map.class)
 	@GetMapping(value = "/emailauth/{id}") // 이메일 인증
-	public Map<String, Integer> emailauth(@ApiParam(value = "String", required = true)@PathVariable String id) throws MessagingException {
+	public ResponseEntity<Map<String, Integer>> emailauth(@ApiParam(value = "String", required = true) @PathVariable String id)
+			throws MessagingException {
 		int confirm = (int) ((Math.random() * (9999 - 1000)) + 1000);
- 		Map<String, Integer> map = new HashMap<>();
-		MimeMessage message = javaMailSender.createMimeMessage();
-		message.setSubject("뉴하 이메일 인증입니다.");
-		message.setRecipient(Message.RecipientType.TO, new InternetAddress(id));
-		message.setText("인증번호: " + confirm);
-		message.setSentDate(new Date());
-		javaMailSender.send(message);
-		map.put("confirm", confirm);
-		return map;
+		HttpStatus status = null;
+		Map<String, Integer> map = new HashMap<>();
+		try {
+			MimeMessage message = javaMailSender.createMimeMessage();
+			message.setSubject("뉴하 이메일 인증입니다.");
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(id));
+			message.setText("인증번호: " + confirm);
+			message.setSentDate(new Date());
+			javaMailSender.send(message);
+			map.put("confirm", confirm);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return new ResponseEntity<Map<String, Integer>>(map, status);
 	}
-	
+
 	@ApiOperation(value = "내가 구독한 큐레이터", notes = "아이디 입력하면 구독한 큐레이터 thumbnail_path, name 리턴", response = ArrayList.class)
 	@GetMapping(value = "/subscribe/{id}") // 내가 구독한 큐레이터
-	public ArrayList<Map<String, String>> subscribe(@ApiParam(value = "String", required = true)@PathVariable String id) {
-		ArrayList<Map<String, String>> list = new ArrayList<Map<String,String>>();
+	public ResponseEntity<ArrayList<Map<String, String>>> subscribe(
+			@ApiParam(value = "String", required = true) @PathVariable String id) {
+		ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
 		int userNo = service.userNo(id);
-		System.out.println(userNo);
-		List<Integer> l = service.follow(userNo);
-		System.out.println(l);
-		for (int i = 0; i < l.size(); i++) {
-			Map<String, String> map = new HashMap<String, String>();
-			User user = service.selectUser(l.get(i));
-			map.put("name", user.getName());
-			map.put("thumbnail_path", user.getThumbnail_path());
-			list.add(map);
+		HttpStatus status = null;
+		try {
+			List<Integer> l = service.follow(userNo);
+			for (int i = 0; i < l.size(); i++) {
+				Map<String, String> map = new HashMap<String, String>();
+				User user = service.selectUser(l.get(i));
+				map.put("name", user.getName());
+				map.put("thumbnail_path", user.getThumbnail_path());
+				list.add(map);
+			}
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		return list;
+		return new ResponseEntity<ArrayList<Map<String, String>>>(list, status);
 	}
-	
-	
+
 	@ApiOperation(value = "회원가입", notes = "회원가입 성공 결과'success' 또는 'fail' 문자열을 리턴", response = Map.class)
 	@PostMapping(value = "/join")
-	public Map<String, String> insert(@ApiParam(value = "User", required = true)@RequestBody User u /*,  @ApiParam(value = "tag List", required = true)@RequestParam List<String> tag*/ ) {
+	public ResponseEntity<Map<String, String>> insert(
+			@ApiParam(value = "User", required = true) @RequestBody User u /*
+																			 * , @ApiParam(value = "tag List", required
+																			 * = true)@RequestParam List<String> tag
+																			 */ ) {
 		Map<String, String> map = new HashMap<>();
 		int result = service.insert(u);
 		int userNo = service.userNo(u.getId());
+		HttpStatus status = null;
 		/*
-		for (int i = 0; i < tag.size(); i++) {
-			service.inserttag(userNo, tag.get(i));
-		}
-		*/
-		System.out.println("userNo:"+userNo);
-		if(result == 0)
+		 * for (int i = 0; i < tag.size(); i++) { service.inserttag(userNo, tag.get(i));
+		 * }
+		 */
+		if (result == 0) {
 			map.put("message", "회원가입 실패");
-		else
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+		else {
 			map.put("message", "회원가입 성공");
-		System.out.println(map);
-		return map;
+			status = HttpStatus.ACCEPTED;
+			}
+		return new ResponseEntity<Map<String, String>>(map, status);
 	}
-	
-	
+
 	@ApiOperation(value = "회원 탈퇴", notes = "탈퇴 결과'success' 또는 'fail' 문자열을 리턴", response = Map.class)
-	@DeleteMapping(value ="/delete/{id}")
-	public String delete(@ApiParam(value = "id", required = true)@PathVariable String id) {
-		service.delete(id);
-		return "삭제완료";
+	@DeleteMapping(value = "/delete/{id}")
+	public ResponseEntity<Map<String, String>> delete(@ApiParam(value = "id", required = true) @PathVariable String id) {
+		Map<String, String> map = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			service.delete(id);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return new ResponseEntity<Map<String, String>>(map, status);
 	}
-	
+
 	@ApiOperation(value = "회원 탈퇴", notes = "수정 결과'success' 또는 'fail' 문자열을 리턴", response = Map.class)
-	@PutMapping(value ="/update")
-	public String update(@ApiParam(value = "User", required = true)@RequestBody User u) {
-		service.update(u);
-		return "수정완료";
+	@PutMapping(value = "/update")
+	public ResponseEntity<Map<String, String>> update(@ApiParam(value = "User", required = true) @RequestBody User u) {
+		Map<String, String> map = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			service.update(u);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, String>>(map, status);
 	}
-	
-	//토근 유효여부 검사
+
+	// 토근 유효여부 검사
 	@ApiOperation(value = "로그인", notes = "'success' 또는 'fail', httpstatus, userInfo 리턴", response = Map.class)
-	@GetMapping(value="/user/{id}")
-	public ResponseEntity<Map<String, Object>> getInfo(
-			@ApiParam(value = "id", required = true)@PathVariable String id,
+	@GetMapping(value = "/user/{id}")
+	public ResponseEntity<Map<String, Object>> getInfo(@ApiParam(value = "id", required = true) @PathVariable String id,
 			HttpServletRequest request) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = HttpStatus.ACCEPTED;
@@ -171,10 +207,11 @@ public class UserController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	//로그인
+
+	// 로그인
 	@ApiOperation(value = "토큰 유효성 검사", notes = "'success' 또는 'fail', token 리턴", response = Map.class)
-	@PostMapping(value="/user/login")
-	public ResponseEntity<Map<String, Object>> login(@ApiParam(value = "id", required = true)@RequestBody User user){
+	@PostMapping(value = "/user/login")
+	public ResponseEntity<Map<String, Object>> login(@ApiParam(value = "id", required = true) @RequestBody User user) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = null;
 		try {
@@ -196,10 +233,11 @@ public class UserController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	//사람검색
+
+	// 사람검색
 	@ApiOperation(value = "유저 검색", notes = "유저 List 리턴", response = List.class)
-	@GetMapping(value="/search/people/{keyword}")
-	public List<User> searchUser(@ApiParam(value = "keyword", required = true)@PathVariable String keyword){
-		return service.searchUser(keyword+"%");
+	@GetMapping(value = "/search/people/{keyword}")
+	public List<User> searchUser(@ApiParam(value = "keyword", required = true) @PathVariable String keyword) {
+		return service.searchUser(keyword + "%");
 	}
 }
