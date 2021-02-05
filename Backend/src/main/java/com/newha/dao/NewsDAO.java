c9633953a2ee0a247ac021960d2f035157eb0537
whiteburnout <whiteburnout@gmail.com>
2021-02-05 17:14:10 +0900

    feature/be/like 충돌 해결

--- Backend/src/main/java/com/newha/dao/NewsDAO.java
+++ Backend/src/main/java/com/newha/dao/NewsDAO.java
@@ -9,6 +9,7 @@ import com.newha.vo.NewsImage;
 import com.newha.vo.Post;
 import com.newha.vo.PostTag;
 import com.newha.vo.User;
+import com.newha.vo.UserGoodNews;
 import com.newha.vo.UserScrapNews;
 
 public interface NewsDAO {
@@ -18,14 +19,24 @@ public interface NewsDAO {
 	public int insertUserScrapNews(UserScrapNews post);
 	public int insertHashTag(String tag);
 	public int insertPostTag(PostTag tag);
+	public int insertUserGoodNews(UserGoodNews userGoodNews);
 	public List<News> selectByUrl(String url);
 	public List<Post> selectPostByName(String name);
 	public List<Post> selectPostById(String id);
 	public User selectUserById(String id);
 	public List<UserScrapNews> selectUserScrapNewsByPostNo(String postNo);
 	public List<HashTag> selectHashTagByName(String name);
+	public UserGoodNews selectLikeNews(UserGoodNews userGoodNews);
+	public UserGoodNews selectDisLikeNews(UserGoodNews userGoodNews);
 	public int deletePost(String postNo);
 	public int deleteScrap(String scarpNo);
 	public int updatePost(Post post);
 	public int updateScrap(UserScrapNews scrap);
+	public int increaseLike(String scrapNo);
+	public int increaseDisLike(String scrapNo);
+	public int decreaseLike(String scrapNo);
+	public int decreaseDisLike(String scrapNo);
+	public int updateLike(UserGoodNews userGoodNews);
+	public int updateDisLike(UserGoodNews userGoodNews);
+	public int updateSave(UserGoodNews userGoodNews);
 }
