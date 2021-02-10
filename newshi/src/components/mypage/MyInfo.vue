6f905020716f00e9be1f23a677bbfe0a16783280
wotjd1163@git config --global user.email kimjea23@naver.com <kimjea23@naver.com>
2021-02-10 16:02:59 +0900

    [S04P13A307-66][Update] feat/마이페이지 : Update 마이페이지UI 및 기능 추가

--- newshi/src/components/mypage/MyInfo.vue
+++ newshi/src/components/mypage/MyInfo.vue
@@ -1,37 +1,54 @@
 <template>
     <v-container>
-      <div class="d-flex justify-center">
-      <v-dialog v-model="dialog" width="500">
-        <template v-slot:activator="{ on, attrs }">
-          <v-avatar size="150px" v-bind="attrs" v-on="on">
-            <v-img :src="member.thumbnail_path"></v-img>
-            <!-- <v-img :src="`${member.thumbnail_path}`" aspect-ratio="1"></v-img> -->
-          </v-avatar>
-        </template>
-        <v-card>
-          <v-card-title>파일 업로드</v-card-title>
-          <v-file-input v-model="file"></v-file-input>
-          <v-btn @click="fileUpload">전송하기!</v-btn>
-        </v-card>
-      </v-dialog>
+      <v-card>
+        <v-row>
+          <v-col>
+            <v-card-actions class="d-flex justify-end">
+            <v-btn class="mr-3"> 비밀번호 수정 </v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div class="d-flex justify-center">
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-avatar size="150px" v-bind="attrs" v-on="on">
+                    <v-img :src="member.thumbnail_path"></v-img>
+                    <!-- <v-img :src="`${member.thumbnail_path}`" aspect-ratio="1"></v-img> -->
+                  </v-avatar>
+                </template>
+              <v-card-title>파일 업로드</v-card-title>
+              <v-file-input v-model="file"></v-file-input>
+              <v-btn @click="fileUpload">전송하기!</v-btn>
+              </v-dialog>
       </div>
-      <div class="d-flex justify-center mt-3" ><h2>{{member.name}}</h2></div>
+      <div class="d-flex justify-center mt-3" >
+        <h2>{{member.name}}</h2>
+      </div>
       <div class="d-flex justify-center">{{member.id}}</div>
-      
-      <v-container >
-        <v-row class="d-flex justify-center mt-10">
-          <v-btn v-for="(hashtag, index) in hashtags" :key="index" text style="font-size:150%" color="#646464">{{hashtag}}</v-btn>
-          <!-- <v-btn text style="font-size:150%" :color="hashtagcolors[index]">{{hashtag}}</v-btn> -->
-          <br v-if="index % 3 == 2"/>
+      <v-container class="ma-5">
+        <v-row>
+          <v-col></v-col>
+          <v-col class="d-flex justify-center" cols="8">
+            <div>
+              <v-btn v-for="(hashtag, index) in hashtags" :key="index" text style="font-size:150%" color="#646464">{{hashtag}}</v-btn>
+            </div>
+          </v-col>
+          <v-col></v-col>
         </v-row>
       </v-container>
-
+        </v-col>
+      </v-row>
+      <v-container>
+      <v-row>
+        <v-col>
       <v-card class="mt-10">
         <v-list style="text-align: left">
-            <v-subheader>팔로워</v-subheader>
+            <v-subheader>구독중인 큐레이터</v-subheader>
             <v-list-item v-for="(follower, index) in followers" :key="index">
               <v-list-item-avatar>
-                <img :src="follower.thumbnail_path">
+                <v-img :src="follower.thumbnail_path"></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title>{{follower.name}}</v-list-item-title>
@@ -41,7 +58,12 @@
             </v-list-item>
         </v-list>
       </v-card>
+        </v-col>
+      </v-row>
+      </v-container>
+      </v-card>
     </v-container>
+    
 </template>
 
 <script>
@@ -77,34 +99,51 @@ export default {
     // this.member.thumbnail_path = "http://images.khan.co.kr/article/2021/02/08/l_2021020802000444700074261.jpg";
     // this.member.name = '로딩중';  
     // this.member.id = '기달';
-    axios.get('http://localhost/sidebarUser/' + 'kimjea23@naver.com'
-    ).then((response) => {
-      console.log(response);
-      this.member.name = response.data.name;
-      // this.member.thumbnail_path = '../../../../../../tmp/' + response.data.thumbnail_path;
-      this.member.id = 'kimjea23@naver.com';
-      this.member.thumbnail_path = "http://images.khan.co.kr/article/2021/02/08/l_2021020802000444700074261.jpg";
-      console.log(this.member.thumbnail_path);
-      console.log(this.member.name)
-      this.$forceUpdate();
-    })
-
-    // this.member = {
-    //     name: '김재성',
-    //     id: 'kimjea23@naver.com',
-    //     thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-    // };
+    // axios.get('http://localhost/sidebarUser/' + 'kimjea23@naver.com'
+    // ).then((response) => {
+    //   console.log(response);
+    //   this.member.name = response.data.name;
+    //   // this.member.thumbnail_path = '../../../../../../tmp/' + response.data.thumbnail_path;
+    //   this.member.id = 'kimjea23@naver.com';
+    //   this.member.thumbnail_path = "http://images.khan.co.kr/article/2021/02/08/l_2021020802000444700074261.jpg";
+    //   console.log(this.member.thumbnail_path);
+    //   console.log(this.member.name)
+    //   this.$forceUpdate();
+    // })
+    console.log("경로>>>>>" + require('@/assets/images/default_avatar.png'));
+    this.member = {
+        name: '김재성',
+        id: 'kimjea23@naver.com',
+        thumbnail_path: require('@/assets/images/default_avatar.png'),
+    };
 
     // 내 hashtags 불러오는 axios(임시)
     this.hashtags = ['#속보', '#정치', '#경제', '#사회', '#문화'];
     // this.hashtagcolors = ['blue', 'red', 'green', 'black', '#6464CD'];
 
-    // 팔로워 불러오는 axios(임시)
-    this.followers = [
-        { thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', id: 'abcde@naver.com', name: '김싸피' },
-        { thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', id: 'fgdsgsdfg@naver.com', name: '이죄송' },
-        { thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', id: 'asdfkl@naver.com', name: '최바보' },
-      ];
+    // 큐레이터 불러오는 axios
+    axios.get('http://localhost:8080/subscribe', 
+      { params: { id: this.member.id } },
+    ).then((response) => { 
+        // 응답 처리 
+        var followers = response.data;
+        console.log(followers);
+        followers.forEach(function(follower) {
+          if(follower.thumbnail_path == null) {
+            follower.thumbnail_path = require('@/assets/images/default_avatar.png');
+          }
+        })
+        this.followers = followers;
+      }) .catch((error) => { 
+        // 예외 처리 
+        console.log(error);
+      })
+
+    // this.followers = [
+    //     { thumbnail_path: require('@/assets/images/default_avatar.png'), id: 'abcde@naver.com', name: '김싸피' },
+    //     { thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', id: 'fgdsgsdfg@naver.com', name: '이죄송' },
+    //     { thumbnail_path: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', id: 'asdfkl@naver.com', name: '최바보' },
+    //   ];
   },
 }
 </script>
