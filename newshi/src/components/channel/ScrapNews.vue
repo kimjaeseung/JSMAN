ab8e191badbb1cf7f94530729a3fc9f0631a2abe
wotjd1163@git config --global user.email kimjea23@naver.com <kimjea23@naver.com>
2021-02-15 14:56:29 +0900

    [S04P13A307-120][ADD] feat/채널 : Update 채널 axios

--- newshi/src/components/channel/ScrapNews.vue
+++ newshi/src/components/channel/ScrapNews.vue
@@ -21,7 +21,7 @@
               <v-row no-gutters>
                 <v-col class="ma-auto" cols=auto>
                   <v-avatar size="120" tile>
-                    <v-img :src="news.image_path"></v-img>
+                    <v-img :src="news.news_image"></v-img>
                   </v-avatar>
                 </v-col>
                 <v-col style="width:0">
@@ -52,6 +52,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -73,28 +74,34 @@ export default {
     this.id = this.$route.params.id;
 
     // 스크랩 기사들 axios
-    this.scrap_news = [
-      {
-        title: '증여세는 시가 기준…증여 직후 거래가격 뛰면 세금 더 낼수도',
-        curator_summary: '기사요약기사요약기사요약기사요약기사요약기사요약기사요약',
-        like_cnt: this.numberWithCommas(12345),
-        dislike_cnt: this.numberWithCommas(6789),
-        date: '2021-02-01 23:43',
-        image_path: 'https://imgnews.pstatic.net/image/001/2021/02/02/PYH2021011914410001300_P4_20210202120232595.jpg?type=w647',
-        islike: true,
-        isdislike: false,
-      },
-      {
-        title: "회삿돈 빼돌려 증여자금 마련…부동산 '아빠찬스' 딱 걸렸네",
-        curator_summary: '기사요약기사요약기사요약기사요약기사요약기사요약기사요약',
-        like_cnt: this.numberWithCommas(23345),
-        dislike_cnt: this.numberWithCommas(67349),
-        date: '2020-12-01 23:43',
-        image_path: 'https://imgnews.pstatic.net/image/025/2021/02/02/0003074770_001_20210202150647925.jpg?type=w647',
-        islike: false,
-        isdislike: true,
-      },
-    ]
+    axios.get('http://localhost:8080/article/scraplist', 
+            { params: { postNo: this.scrap_no } },
+          ).then((response) => {
+            this.scrap_news = response.data;
+          });
+
+    // this.scrap_news = [
+    //   {
+    //     title: '증여세는 시가 기준…증여 직후 거래가격 뛰면 세금 더 낼수도',
+    //     curator_summary: '기사요약기사요약기사요약기사요약기사요약기사요약기사요약',
+    //     like_cnt: this.numberWithCommas(12345),
+    //     dislike_cnt: this.numberWithCommas(6789),
+    //     date: '2021-02-01 23:43',
+    //     image_path: 'https://imgnews.pstatic.net/image/001/2021/02/02/PYH2021011914410001300_P4_20210202120232595.jpg?type=w647',
+    //     islike: true,
+    //     isdislike: false,
+    //   },
+    //   {
+    //     title: "회삿돈 빼돌려 증여자금 마련…부동산 '아빠찬스' 딱 걸렸네",
+    //     curator_summary: '기사요약기사요약기사요약기사요약기사요약기사요약기사요약',
+    //     like_cnt: this.numberWithCommas(23345),
+    //     dislike_cnt: this.numberWithCommas(67349),
+    //     date: '2020-12-01 23:43',
+    //     image_path: 'https://imgnews.pstatic.net/image/025/2021/02/02/0003074770_001_20210202150647925.jpg?type=w647',
+    //     islike: false,
+    //     isdislike: true,
+    //   },
+    // ]
   },
 }
 </script>
